8d3f364149e4d04d3a6cd4eb6b47af1a793aebda
ahmed BEN-JMII <ahmed.ben-jmii@talan.com>
2021-05-29 16:14:19 +0100

    add won method

--- main.rb
+++ main.rb
@@ -1,3 +1,4 @@
+board = ["" ,"" ,"" ,"" ,"" ,"" ,"" ,"" ,""]
 
 WIN_COMBINATIONS = [
   [0,1,2],
@@ -9,3 +10,30 @@ WIN_COMBINATIONS = [
   [0,4,8],
   [2,4,6]
 ]
+
+def won(board, symbol)
+  for win_combination in WIN_COMBINATIONS
+  board_index_0 = board[win_combination[0]]
+    board_index_1 = board[win_combination[1]]
+    board_index_2 = board[win_combination[2]]
+
+    if board_index_0 == symbol && board_index_1 == symbol && board_index_2 == symbol
+      return true
+    end
+  end
+  return false
+end
+
+def full(board)
+
+end
+
+puts board.empty?
+puts "board_length = " + board.length.to_s
+
+while (true)
+
+  # query players for moves
+  # check if anyone has won ==> If yes break declaring the winner of the game
+  # check if full(board) ==> If yes break declaring tie
+end
